24f2fa7adbcaf59f12d58716404d476e6f207fa1
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2020-03-03 13:38:19 +0200

    :art: added title with react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
 import "../styles/styles.scss"
+import { Helmet } from "react-helmet"
 
 //import components
 import Content from "../components/content"
 
 const IndexPage = () => (
   <div className="main">
-    <title>Dor Lugasi-Gal</title>
-
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Dor Lugasi-Gal | Software Engineer</title>
+    </Helmet>
     <Content />
   </div>
 )
