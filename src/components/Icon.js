1d439c12c2da62bb60edd17e4e24df1ad010c13b
Dor Lugasi-Gal <dorlugasigal@gmail.com>
2024-09-30 12:33:57 +0300

    add devblog

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -18,6 +18,7 @@ export default function Icon(props) {
     linkedIn: FontAwesome.FaLinkedinIn,
     mail: FiMail,
     whatsapp: FontAwesome.FaWhatsapp,
+    microsoft: FontAwesome.FaMicrosoft,
     default: FontAwesome.FaQuestion,
   }
   const toggleHover = () => {
